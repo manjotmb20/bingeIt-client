0c7b232e1f72a02334ff827e26e8d833a2674b3a
krithikaiyer99 <iyer.kr@northeastern.edu>
2023-04-13 14:00:46 -0400

    add trailer component

--- src/home/index.js
+++ src/home/index.js
@@ -1,34 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getPopularMovies } from "../store";
+import { getPopularMovies, getLatest } from "../store";
 import SliderComponent from "./SliderComponent";
-import SimpleSliderComponent from "./SimpleSliderComponent"
 import "./index.css";
 
 const HomeComponent = () => {
   const popularmovies = useSelector((state) => state.bingeit.popularmovies);
   const dispatch = useDispatch();
-
+  const latestMovie = useSelector((state) => state.bingeit.latest);
+  const [trailer, setTrailer] = useState(null);
+  const getTrailer = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${latestMovie.id}/videos?api_key=8ed01ac7fe8bdfc25206f1bcbd4d22ab`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const ytTrailer = data.results.find(
+          (video) => video.site === "YouTube" && video.type === "Trailer"
+        );
+        if (ytTrailer) {
+          setTrailer(ytTrailer.key);
+        }
+      });
+  };
   useEffect(() => {
+    dispatch(getLatest());
+    getTrailer();
     dispatch(getPopularMovies());
-  }, []);
-
+  }, [latestMovie,trailer]);
   return (
     <div className="home-slider">
-      <SimpleSliderComponent movies={popularmovies}/>
+      {latestMovie && (
+        <div className="video-container">
+          <div className="buttons-container">
+            <a href="/play" className="play-button">
+              <i className="fas fa-play"></i> Play
+            </a>
+            <a href="/info" className="info-button">
+              <i className="fas fa-info-circle"></i> Info
+            </a>
+          </div>
+          <iframe
+            title="trailer"
+            width="100%"
+            height="100%"
+            src={`https://www.youtube.com/embed/${trailer}?autoplay=1`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
+      )}
       <h2 className="text-white">What's Popular</h2>
       <SliderComponent movies={popularmovies} />
       <h2 className="text-white">Trending This Week</h2>
       <SliderComponent movies={popularmovies} />
       <h2 className="text-white">Top 10 TV Shows Today</h2>
-      <SliderComponent movies={popularmovies} numbering={true}/>
-      <h2 className="text-white">New Releases</h2>
       <SliderComponent movies={popularmovies} />
       <h2 className="text-white">My List</h2>
-      <SliderComponent movies={popularmovies} />
-      <h2 className="text-white">Action & Adventure</h2>
-      <SliderComponent movies={popularmovies} />
-      <h2 className="text-white">Romantic Favorites</h2>
       <SliderComponent movies={popularmovies} />
     </div>
   );
