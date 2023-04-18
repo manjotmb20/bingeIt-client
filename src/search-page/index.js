acdc755ffea6e1feb2d5cc795ac5e9f87db607d4
krithikaiyer99 <iyer.kr@northeastern.edu>
2023-04-18 16:33:53 -0400

    add search keywords

--- src/search-page/index.js
+++ src/search-page/index.js
@@ -1,22 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams } from "react-router-dom";
 import "./index.css";
 
 const SearchPage = () => {
   const [searchParams] = useSearchParams();
   const [searchResults, setSearchResults] = useState([]);
-  const query = searchParams.get("query"); 
+  const [searchKeywords, setSearchKeywords] = useState([]);
+  const query = searchParams.get("query");
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=3d39d6bfe362592e6aa293f01fbcf9b9&query=${query}`)
-    .then((res) => res.json())
-    .then((data) => {
-        setSearchResults(data.results)
-    });
-  }, []);
-  console.log(searchResults)
+    fetch(
+      `https://api.themoviedb.org/3/search/multi?api_key=3d39d6bfe362592e6aa293f01fbcf9b9&query=${query}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setSearchResults(data.results.filter((movie) => movie.poster_path));
+      });
+  }, [query]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/search/keyword?api_key=3d39d6bfe362592e6aa293f01fbcf9b9&query=${query}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setSearchKeywords(data.results);
+      });
+  }, [searchKeywords]);
+  console.log(searchKeywords)
   return (
     <div className="search-page">
-      <h2 className="movie-title">hi</h2>
+      {searchKeywords.length > 0 && (
+        <div className="search-keywords">
+          Explore titles related to:{" "} 
+          {searchKeywords.map((keyword, index) => (
+            <span key={index}>
+              {keyword.name}
+              {index !== searchKeywords.length - 1 && " | "}
+            </span>
+          ))}
+        </div>
+      )}
       <div className="movie-grid">
         {searchResults.map((movie) => (
           <div key={movie.id} className="movie-card">
